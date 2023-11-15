348650a186999a9d8d9770d24a88731605af7319
SaintEduis <edik.melk@yandex.ru>
2023-11-15 14:02:23 +0300

    Поменял вывод!

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello world!");
+        System.out.println("Hi!");
         int num = in.nextInt();
         int num1 = in.nextInt();
 
